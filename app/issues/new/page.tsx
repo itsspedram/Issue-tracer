3ee39abb8670ce68724125ecfc3f8ab02f38b0fe
itsspedram <pedmsv@gmail.com>
2024-07-31 20:01:43 +0330

    handel errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,34 @@
 "use client";
-import { TextField, Button } from "@radix-ui/themes";
+import { TextField, Button, Callout } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface issueForm {
   title: string;
   description: string;
 }
 const NewIssuePage = () => {
+  const [err,setErr]=useState("")
   const { register, control, handleSubmit } = useForm<issueForm>();
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <>
+    <div className="max-w-xl">
+      {err&&<Callout.Root color="red" className="my-5">
+        <Callout.Text>{err}</Callout.Text>
+      </Callout.Root>}
       <form
-        className="w-[700px] mt-2"
+        className=" mt-2"
         onSubmit={handleSubmit(async (d) => {
-          await axios.post("/api/issues", d);
-          router.push('/')
+          try {
+            await axios.post("/api/issues", d);
+            router.push("/");
+          } catch (error) {
+              setErr("oooooooooooooooooooooooooops!")
+          }
         })}
       >
         <TextField.Root
@@ -39,7 +48,7 @@ const NewIssuePage = () => {
         />
         <Button className="!px-7">create</Button>
       </form>
-    </>
+    </div>
   );
 };
 
